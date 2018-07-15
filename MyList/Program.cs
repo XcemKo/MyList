bf30082ace2e6f20696dd50fd5b4ba253c38cfea
XcemKo <Xcem2010gmail.com>
2018-07-15 22:52:57 +0500

    ADd FIles

---
+++ MyList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main()
+        {
+            myList<int> test = new myList<int>();
+
+        }
+    }
+}
